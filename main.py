f7f974bf242b9bc6a18d5b749ceb96444828d658
Viktor Persson <viktor.persson@arcsin.se>
2021-01-17 15:47:09 +0100

    Add basic example.

--- main.py
+++ main.py
@@ -1,32 +1,52 @@
 import os
-
 import discord
-from discord.ext import commands
-from discord_slash import cog_ext, SlashCommand, SlashContext
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-slash = SlashCommand(bot)
+client = discord.Client()
 
 
-@slash.slash(name="slashtest")
-async def _test(ctx: SlashContext):
-    embed = discord.Embed(title="embed test")
-    await ctx.send(content="test", embeds=[embed])
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
 
 
-class ServerCog(commands.Cog):
-    def __init__(self, bot):
-        if not hasattr(bot, "slash"):
-            # Creates new SlashCommand instance to bot if bot doesn't have.
-            bot.slash = SlashCommand(bot, override_type=True)
-        self.bot = bot
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    @cog_ext.cog_slash(name="cogtest")
-    async def _test(self, ctx: SlashContext):
-        embed = discord.Embed(title="embed test")
-        await ctx.send(content="test", embeds=[embed])
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
 
 
-bot.add_cog(ServerCog(bot))
-bot_token = os.environ.get("DISCORD_BOT_TOKEN")
-bot.run(bot_token)
+client.run(os.environ.get("DISCORD_BOT_TOKEN"))
+
+# import discord
+# from discord.ext import commands
+# from discord_slash import cog_ext, SlashCommand, SlashContext
+#
+# bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+# slash = SlashCommand(bot)
+#
+#
+# @slash.slash(name="slashtest")
+# async def _test(ctx: SlashContext):
+#     embed = discord.Embed(title="embed test")
+#     await ctx.send(content="test", embeds=[embed])
+#
+#
+# class ServerCog(commands.Cog):
+#     def __init__(self, bot):
+#         if not hasattr(bot, "slash"):
+#             # Creates new SlashCommand instance to bot if bot doesn't have.
+#             bot.slash = SlashCommand(bot, override_type=True)
+#         self.bot = bot
+#
+#     @cog_ext.cog_slash(name="cogtest")
+#     async def _test(self, ctx: SlashContext):
+#         embed = discord.Embed(title="embed test")
+#         await ctx.send(content="test", embeds=[embed])
+#
+#
+# bot.add_cog(ServerCog(bot))
+# bot_token = os.environ.get("DISCORD_BOT_TOKEN")
+# bot.run(bot_token)
