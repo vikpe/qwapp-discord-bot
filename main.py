7cf7aaa997a663f1b56ce9a7a1ea8c6a519f6ca0
Viktor Persson <viktor.persson@arcsin.se>
2022-04-07 19:40:06 +0200

    Update/reset bot to latest discord.py version.

--- main.py
+++ main.py
@@ -1,42 +1,16 @@
 import os
-
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-
-
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user.name} ({bot.user.id})")
-
-
-@bot.group()
-async def servers(ctx):
-    if ctx.invoked_subcommand is None:
-        await ctx.send("Invalid server command passed...")
-
-
-@servers.command(help="List servers")
-async def list(ctx) -> None:
-    msg = "[list servers]"
-    await ctx.send(msg)
-
-
-@servers.command(help="Start server")
-async def start(ctx, aws_region: str) -> None:
-    msg = f"[start server in {aws_region}]"
-    await ctx.send(msg)
-
-
-@servers.command(help="Stop server")
-async def stop(ctx, server_id: str) -> None:
-    msg = f"[stop server by id {server_id}]"
-    await ctx.send(msg)
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='>', intents=intents)
 
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send("pong")
+    await ctx.send('pong')
 
 
-bot.run(os.environ.get("DISCORD_BOT_TOKEN"))
+bot_token = os.environ.get("DISCORD_BOT_TOKEN")
+bot.run(bot_token)
